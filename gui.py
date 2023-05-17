ad3160a40b9172809c00b9b7e8433ebb8fd974b0
andreibindasov <bindasov.andrei@gmail.com>
2023-05-17 16:18:39 -0600

    gui PySimpleGui Step 1 update

--- gui.py
+++ gui.py
@@ -3,9 +3,9 @@ import PySimpleGUI as psg
 
 label = psg.Text("Enter a to-do")
 input_box = psg.InputText(tooltip="Enter todo")
-
+input_box2 = psg.InputText(tooltip="Enter todo")
 add_btn = psg.Button("Add")
 
-window = psg.Window("To-Do App", layout=[[label], [input_box, add_btn]])
+window = psg.Window("To-Do App", layout=[[label], [input_box], [add_btn]])
 window.read()
 window.close()
