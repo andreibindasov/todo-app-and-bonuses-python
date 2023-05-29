3e438e66c94657bcd19b81d25f6f2acd9850fbef
andreibindasov <bindasov.andrei@gmail.com>
2023-05-29 15:18:39 -0600

    add todo feature

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ import modules.functions as mf
 
 todos = mf.get_todos()
 
+
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    mf.update_todos(todos)
+
+
 st.title("To-Do App")
 st.subheader("Put your plans in order!")
 st.write("With this App you will increase your productivity.")
@@ -10,4 +17,5 @@ st.write("With this App you will increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a to-do...")
+st.text_input(label="", placeholder="Add a to-do...",
+              on_change=add_todo, key='new_todo')
