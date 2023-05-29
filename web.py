90fe8e641725531ae2bbd8f86ed07e58db94b8e2
andreibindasov <bindasov.andrei@gmail.com>
2023-05-29 15:26:54 -0600

    completed feature added

--- web.py
+++ web.py
@@ -8,14 +8,23 @@ def add_todo():
     todo = st.session_state['new_todo'] + "\n"
     todos.append(todo)
     mf.update_todos(todos)
+    st.session_state["new_todo"] = ""
 
 
 st.title("To-Do App")
 st.subheader("Put your plans in order!")
 st.write("With this App you will increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    chkbox = st.checkbox(todo, key=todo)
+    if chkbox:
+        todos.pop(index)
+        mf.update_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add a to-do...",
               on_change=add_todo, key='new_todo')
+
+
+# st.session_state
